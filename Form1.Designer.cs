0c4f05a8c6023b8b60f62b7af79a0ea896895268
viXa221 <vixa2122@gmail.om>
2025-04-24 15:28:55 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace kursova_rabota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnRemoveLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(162, 103);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 26);
+            this.txtTitle.TabIndex = 0;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(71, 379);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(491, 144);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Заглавие";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Автор";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Жанр";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Година";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(220, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Онлайн Библиотека";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(162, 143);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 26);
+            this.txtAuthor.TabIndex = 7;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(162, 193);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(100, 26);
+            this.txtGenre.TabIndex = 8;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(162, 237);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 26);
+            this.txtYear.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(354, 98);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(148, 30);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Добави книга";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(354, 155);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(213, 29);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Изтрий избраната книга";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(354, 209);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(213, 27);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Търси книга по заглавие";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnRemoveLast
+            // 
+            this.btnRemoveLast.Location = new System.Drawing.Point(354, 262);
+            this.btnRemoveLast.Name = "btnRemoveLast";
+            this.btnRemoveLast.Size = new System.Drawing.Size(234, 35);
+            this.btnRemoveLast.TabIndex = 13;
+            this.btnRemoveLast.Text = "Отмени последно добавяне";
+            this.btnRemoveLast.UseVisualStyleBackColor = true;
+            this.btnRemoveLast.Click += new System.EventHandler(this.btnRemoveLast_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 567);
+            this.Controls.Add(this.btnRemoveLast);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
+            this.Text = "Управление на книги ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnRemoveLast;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursova_rabota
+{
+    public partial class Form1 : Form
+    {
+            //Деклариране на структурите
+        private List<Book> books = new List<Book>();// това е основният списък (за съхранение на всички книги)
+        private Stack<Book> lastAddedBooks = new Stack<Book>();//това е списъка за отмяна на последно добавената книга
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book//създавам отделен клас Book, който дефинира стуктурта от данни на една книга със свойставата долу:
+        {
+            public string Title { get; set; }//това са свойствата на книгата (property)
+            public string Author { get; set; }
+            public int Year { get; set; }
+            public string Genre { get; set; }
+
+            public override string ToString()
+
+                //с това поакзвам книгата в четим вид в списъка
+            {
+                return $"{Title} е написан от: {Author}, през ({Year}) година. Жанра е - {Genre}";//когато книгата е добавена в listBox-a, излиза това съобщение       
+            }//Класът Book има метод ToString и затова книгата ще се покаже така
+        }
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text.Trim();
+            string author = txtAuthor.Text.Trim();
+            string genre = txtGenre.Text.Trim();
+            int year;
+
+            if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(author) || string.IsNullOrWhiteSpace(genre))
+            //проверка на празни полета
+            {
+                MessageBox.Show("Моля, попълни всики полета");//ако някое поле е празно
+                return;
+            }
+            if (!int.TryParse(txtYear.Text, out year))//опитва се да превърне txtYear в цяло число
+              //проверка, дали годината е число
+            {
+                MessageBox.Show("Годината трябва да е число!");
+                return;
+            }
+            
+       
+
+                Book newBook = new Book//създавам си нов обект от класа Book
+                {//създава нова книга с въведените стойности, 
+                    Title = title,
+                    Author = author,
+                    Genre = genre,
+                    Year = year
+                    //стойностите, които всяка книга има
+                };
+            books.Add(newBook);//добавяме новата книга в списъка. books е от List списъка
+            lastAddedBooks.Push(newBook);//това е от Стек структура и се добавя И тук, за да можем когато решим да я премахнем (работи като Undo)
+
+            UpdateBookList();
+            ClearTextFields();
+
+            MessageBox.Show("Книгата е добавена успешно!");
+            //return;
+        }
+       
+
+        private void btnDelete_Click(object sender, EventArgs e)//изтриване на книга
+        {
+            int selectedIndex = listBox1.SelectedIndex;//взима индекса на избрания елемент от списъка
+            if(selectedIndex >= 0 && selectedIndex < books.Count)//проверява дали елемента съществува, ако е валиден избраноя индекс
+            {
+                books.RemoveAt(selectedIndex);//се премахва книгата от списъка books, на съответния индексс
+                UpdateBookList();// обновяваме
+            }
+            else//ако не сме избрали книга
+            {
+                MessageBox.Show("Моля, изберете книга от списъка!");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchTitle = txtTitle.Text.Trim().ToLower();//търсене на книга по заглавие
+            Book found = books.Find(b => b.Title.ToLower().Contains(searchTitle));
+            if(found != null)//
+            {
+                MessageBox.Show($"Намерена книга:\n{found}");
+            }
+            else//ако е null, книгата не е намерена:
+            {
+                MessageBox.Show("Книгата със зададеното заглавие не е намерена");
+            }
+        }
+
+        private void btnRemoveLast_Click(object sender, EventArgs e)
+        {
+            if(lastAddedBooks.Count > 0)
+                // проверява дали има добавена книга за премахване
+            {
+                Book last = lastAddedBooks.Pop();//премахваме последната добавена книга в списъка
+                //тук използвам и двете структури от данни, защото в случая е за изтриване на книга,
+                //когато изтривам книга, тя е била запазена и в двете структури,
+                //затова се взима по индекс от List-a, и после от Stack-a.
+                //тук Book е от списъка на List
+                //проверява коя е последно добавената книга
+                books.Remove(last);//книгата се премахва
+                UpdateBookList();//и BookList-а се рефрешва
+            }
+            else//ако няма добавени книги
+            {
+                MessageBox.Show("Няма последно обавена книга за отмяна");
+            }
+        }
+        private void UpdateBookList()
+        {
+            listBox1.Items.Clear();//изтрива всички ккниги, които са в ListBox-a
+            foreach(Book b in books)//обхождаме всички книги 
+            {
+                listBox1.Items.Add(b);//добавяме всяка книга в списъка (ListBox1)
+            }
+        }
+        private void ClearTextFields()//
+            //декларирам си метод, който да изчиства полетата, когота книгата се добави
+        {
+            txtTitle.Clear();
+            txtAuthor.Clear();
+            txtGenre.Clear();
+            txtYear.Clear();
+        }
+    }
+}
